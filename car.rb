b81d8660e70a42432da65fa821dc5487ec23689c
Your Name <ts@ts-VPCEG1BFX.(none)>
2011-09-08 12:15:55 -0400

    commit fix

--- car.rb
+++ car.rb
@@ -1,13 +1,15 @@
 class Car
-otter_accessor :number_of_doors
-def number_of_people
-if @number_of_doors == 2 then
-2
-elsif @number_of_doors == 4 then
-5
-else
-nil
-end
-end
+  
+  attr_accessor :number_of_doors
+  
+  def number_of_people
+    if @number_of_doors == 2 then
+      2
+    elsif @number_of_doors == 4 then
+      5
+    else
+      nil
+    end
+  end
 end
 
